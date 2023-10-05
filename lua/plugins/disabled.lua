a740f1a8f7732cea6f78f4ec17d11f3642cacdc5
Jaroslaw Surkont <jaroslaw.surkont@unibas.ch>
2023-10-05 13:34:00 +0200

    fix: disable folke/flash

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -3,4 +3,5 @@ return {
   { "ggandor/leap.nvim", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
   { "folke/noice.nvim", enabled = false },
+  { "folke/flash.nvim", enabled = false },
 }
